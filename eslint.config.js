46a5029e821a554d8858a8bf3b0e903c3e732f04
jinoo <jinoo0306@naver.com>
2024-11-13 23:12:20 +0900

    chore : eslint 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-)
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -7,22 +8,23 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // '@/path'로 'src/path'에 접근할 수 있도록 별칭 설정
-    }
+      '@': path.resolve(__dirname, 'src'), // '@/path'로 'src/path'에 접근할 수 있도록 별칭 설정
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables.scss";` // 공통 SCSS 파일을 모든 SCSS 파일에 자동으로 추가
-      }
-    }
+        // 모든 SCSS 파일에 _variables.scss 파일 자동으로 추가
+        additionalData: `@import "@/assets/styles/abstracts/_variables.scss";`,
+      },
+    },
   },
   server: {
     open: true, // 개발 서버 시작 시 브라우저 자동으로 열기
-    port: 3000 // 개발 서버 포트 설정 (기본값: 3000)
+    port: 3000, // 개발 서버 포트 설정 (기본값: 3000)
   },
   build: {
     sourcemap: true, // 소스 맵 생성으로 디버깅 편의성 향상
-    outDir: 'dist' // 빌드 결과물이 저장될 폴더 이름
-  }
+    outDir: 'dist', // 빌드 결과물이 저장될 폴더 이름
+  },
 });
