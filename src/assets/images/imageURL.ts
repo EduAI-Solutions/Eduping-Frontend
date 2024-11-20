01cf1ebefa1359baf6cebf06104976a536f7f014
jinoo <jinoo0306@naver.com>
2024-11-20 23:29:43 +0900

    design : favicon 설정

---
+++ src/assets/images/imageURL.ts
@@ -0,0 +1,6 @@
+export const LOGO = {
+  INTRO: 'https://i.ibb.co/LYj8Xd2/intro-logo.png',
+  FAVICON: 'https://i.ibb.co/hYB71xM/favicon.png',
+  FAVICON_PRIMARY: 'https://i.ibb.co/x3Bpz4Z/favicon-violet-nobg.png',
+  FAVICON_SECONDARY: 'https://i.ibb.co/kMGBXv5/favicon-white-nobg.png',
+};
