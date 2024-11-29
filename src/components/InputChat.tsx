741e097c57ff78af49104e626697da866a45426b
jinoo <jinoo0306@naver.com>
2024-11-29 23:39:04 +0900

    design : input 컴포넌트 임시 퍼블리싱

--- src/components/InputChat.tsx
+++ src/components/InputChat.tsx
@@ -1,7 +1,14 @@
 import styles from './InputChat.module.scss';
 
+interface InputChatProps {
+  case?: 'default' | 'hover' | 'focus' | 'filled' | 'disabled' | 'generate';
+  inputValue: string;
+  setInputValue: (value: string) => void;
+  onClick?: () => void;
+}
+
 const InputChat = () => {
-  return <div className={styles.container}></div>;
+  return <div className={styles.container}>ㅁㄴㅇㄹ</div>;
 };
 
 export default InputChat;
