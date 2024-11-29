a98b6962d6ab8cad9b3dcd58a92d2f9bc1681086
jinoo <jinoo0306@naver.com>
2024-11-29 23:39:19 +0900

    design : 랜딩 페이지 초기 구성

--- src/pages/Landing/page.tsx
+++ src/pages/Landing/page.tsx
@@ -1,8 +1,9 @@
 import InputChat from '@/components/InputChat';
+import styles from './page.module.scss';
 
 const Landing = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Welcome to Chat App</h1>
       <InputChat />
     </div>
