9832e1305e21da66e5db5248aac3c091a940963f
jinoo <jinoo0306@naver.com>
2024-11-30 21:16:36 +0900

    design : 랜딩페이지 퍼블리싱

--- src/pages/Landing/page.tsx
+++ src/pages/Landing/page.tsx
@@ -1,6 +1,7 @@
 import InputChat from '@/components/InputChat';
 import styles from './page.module.scss';
 import { useState } from 'react';
+import Button from '@/components/Button';
 
 interface InputValueType {
   text: string; // 텍스트 입력 값
@@ -29,7 +30,32 @@ const Landing = () => {
   };
   return (
     <div className={styles.container}>
-      <h1>Welcome to Chat App</h1>
+      <div className={styles.subcontainer}>
+        <h1 className={styles.title}>
+          안녕하세요, <br /> '{'조진우'}' 선생님! 어떤 업무 보조가 필요하신가요?
+        </h1>
+        <p className={styles.subtitle}>
+          아래 업무 보조 유형 중 원하는 유형을 선택하거나, 직접 프롬프트를
+          입력해보세요.
+        </p>
+        <div className={styles.button_container}>
+          <Button color="secondary" size="full">
+            생기부 요약
+          </Button>
+
+          <Button color="secondary" size="full">
+            생기부 요약
+          </Button>
+
+          <Button color="secondary" size="full">
+            생기부 요약
+          </Button>
+
+          <Button color="secondary" size="full">
+            생기부 요약
+          </Button>
+        </div>
+      </div>
       <InputChat
         status="default"
         inputValue={inputValue}
